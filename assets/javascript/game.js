2954ab9e89626abff79e0545d4f72c5a5bcabf01
MDTrue <MDTrueII@hotmail.com>
2018-11-08 02:40:36 -0800

    basic framework for psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+    var wins = 0;
+    var losses = 0;
+    var chances = 10;
+    var guesses =0;
+
+
+    var randomChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+
+    console.log(randomChoice)
+
+    document.onkeyup = function(event){
+        var userGuess = event.key;
+
+        if(userGuess === randomChoice){
+            wins++
+        
+        }
+        else{
+            guesses --;
+        }
+            if(guesses === 0){
+                losses++
+            }
+            document.getElementById("wins").innerHTML =  wins;
+            document.getElementById("losses").innerHTML = losses;
+            document.getElementById("chances").innerHTML = chances;
+            document.getElementById("guesses").innerHTML = guesses;
+            
+
+    }
